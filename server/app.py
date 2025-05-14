aebff616ab56f2e575d2e52a42c530568b388bd2
whan0767 <whan0767@naver/com>
2025-05-14 23:17:54 +0900

    modify file structure

--- server/app.py
+++ server/app.py
@@ -2,22 +2,23 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from factchecker import analyze_comment
 from services.api import extract_keywords_batch_llm
-from _factchecker import CommentFactCheck
+from server.factcheck_engine import CommentFactCheck
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
 
     data = request.get_json()
     comment = data["comment"]
-    video_title   = data.get("title", "")
+    video_title = data.get("title", "")
     video_description = data.get("description", "")
-    video_hashtags   = data.get("hashtags", [])
-    fact_result, article_info = analyze_comment(comment)
+    video_hashtags = data.get("hashtags", [])
 
     factchecker = CommentFactCheck(comment)
     factchecker.analyze()
@@ -36,13 +37,15 @@ def analyze():
 
     return jsonify(response)
 
+
 @app.route("/batch_extract", methods=["POST"])
 def batch_extract():
-    data       = request.get_json()
-    comments   = data["comments"]
-    video_ctx  = data.get("videoContext", {})       # 메타데이터 함께 받음
+    data = request.get_json()
+    comments = data["comments"]
+    video_ctx = data.get("videoContext", {})  # 메타데이터 함께 받음
     results = extract_keywords_batch_llm(comments, video_ctx, n=6)
     return jsonify(results)
+
 
 # get fact-check result for all comemnts
 @app.route("/factcheck", methods=["POST"])
@@ -73,6 +76,7 @@ def analyze_comments():
         }
         response["data"].append(result)
     return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
